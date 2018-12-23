1d818d4367f1ef5003751e8bc3f5246bc9606db2
SzymG <szymon.gagorek@gmail.com>
2018-12-23 23:58:22 +0100

    Settings activity working properly, no functionality instead of back

--- app/src/main/java/com/example/szymo/ajjkalamba/SettingsActivity.java
+++ app/src/main/java/com/example/szymo/ajjkalamba/SettingsActivity.java
@@ -2,12 +2,34 @@ package com.example.szymo.ajjkalamba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Button powrot, edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        edit = (Button) findViewById(R.id.button5);
+        powrot = (Button) findViewById(R.id.button6);
+
+        powrot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void finish(){
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }
